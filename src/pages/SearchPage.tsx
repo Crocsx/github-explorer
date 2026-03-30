f36f53df54eefbe240f0840572306e12c18cd5e6
Crocsx <crocsx.gaming@gmail.com>
2026-03-30 19:43:34 +0900

    feat: implement pagination

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,7 +1,7 @@
 import { useState, useCallback } from 'react';
 
-import { TextInput, Button, AppShell, Flex } from '@mantine/core';
-import { useDebouncedValue } from '@mantine/hooks';
+import { TextInput, Button, AppShell, Flex, Pagination } from '@mantine/core';
+import { useDebouncedValue, useScrollIntoView } from '@mantine/hooks';
 
 import {
   QueryBoundary,
@@ -11,20 +11,34 @@ import {
   RepositoryList,
   RepositoryListLoader,
 } from '@/components';
+import { PER_PAGE } from '@/config/repository';
 import { useRepoSearch } from '@/hooks/useRepoSearch';
 
 export const SearchPage = () => {
+  const [page, setPage] = useState(1);
   const [inputValue, setInputValue] = useState('');
   const [debouncedQuery] = useDebouncedValue(inputValue, 500);
-
+  const { scrollIntoView, targetRef } = useScrollIntoView({
+    offset: 60,
+    duration: 50,
+  });
   const { data, isError, isLoading, isRefetching, error } = useRepoSearch(
     debouncedQuery,
-    1,
+    page,
+    { perPage: PER_PAGE },
   );
 
   const handleSearch = useCallback(() => {
     setInputValue(inputValue);
   }, [inputValue]);
+
+  const handlePageChange = useCallback(
+    (newPage: number) => {
+      scrollIntoView();
+      setPage(newPage);
+    },
+    [scrollIntoView],
+  );
 
   return (
     <AppShell header={{ height: 48 }}>
@@ -40,22 +54,29 @@ export const SearchPage = () => {
               onChange={(e) => setInputValue(e.target.value)}
               onKeyDown={handleSearch}
               flex={1}
+              ref={targetRef}
             />
             <Button onClick={handleSearch} loading={isLoading || isRefetching}>
               Search
             </Button>
           </Flex>
           <QueryBoundary
-            isLoading={isLoading}
+            isLoading={isLoading || isRefetching}
             data={data}
             isError={isError}
             isEmpty={data?.items.length === 0}
             emptyFallback={<EmptyState />}
             errorFallback={<ErrorAlert message={error?.message} />}
-            loadingFallback={<RepositoryListLoader />}
+            loadingFallback={<RepositoryListLoader count={PER_PAGE} />}
           >
             {(result) => <RepositoryList repos={result.items} />}
           </QueryBoundary>
+          <Pagination
+            disabled={isLoading || isRefetching}
+            total={data?.total_count || 0}
+            value={page}
+            onChange={handlePageChange}
+          />
         </Flex>
       </AppShell.Main>
     </AppShell>
